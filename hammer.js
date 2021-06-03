75464efebde4cd6d936f64d7622174c30e59d8d9
sidhuPaji-777 <82391641+sidhuPaji-777@users.noreply.github.com>
2021-06-03 15:54:33 +0530

    Update hammer.js
    updated

--- hammer.js
+++ hammer.js
@@ -1,38 +1,42 @@
 class hammer
-{
-    constructor(x,y,width,height, option)
+{   
+    constructor(x, y) 
     {
-        // this.option={
-        //     // 'restitution' : 0.8,
-        //     //  "isStatic": true
-        // }
-        this.body=Bodies.rectangle(x,y,width,height,option);
-        World.add(world,this.body);
+        var options = 
+        { 
+            'density':2,
+            'friction': 1.0,
+            'restitution':0.5 
+        };
         
-        this.width=width;
-        this.height=height;
-
-        this.image = loadImage("sprites/Hammer.png");
-        // this.image.scale = 1;
-        this.body.angle = -20;
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = 100;
+        this.height = 40;
+        World.add(world, this.body);
+        
+        // this.image = loadImage("sprites/Hammer.png");
+        // this.body.angle = 2;
     }
-    
+
     display()
-    {
-        var pos=this.body.position;
-        rectMode(CENTER);
-        rect(pos.x,pos.y,this.width,this.height);    
-        this.positionX = mouseX;
-        this.positionX = mouseX;
+    { 
+        var pos = this.body.position;
+        pos.x = mouseX;
+        pos.y = mouseY;
 
         var angle = this.body.angle;
         push();
-        translate(this.body.position.x, this.body.position.y);
+        translate(pos.x, pos.y);
         rotate(angle);
-        imageMode(CENTER);
-        image(this.image, 10, 0, 150, 40);
+        strokeWeight(3);
+        stroke('white');
+        fill('orange');
+        rectMode(CENTER)
+        rect(0, 0, this.width, this.height);
         pop();
+
+        // imageMode(CENTER);
+        // image(this.image, pos.x, pos.y, 150, 40);
     }
 
-    
 }
